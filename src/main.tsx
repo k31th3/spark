6ea7459a9fa83ed2f76e10b9b43917564ef9e629
Keith3 <keith3deyta@gmail.com>
2026-07-29 07:15:07 +0800

    inter-variable
    - replace inter old to inter-variable

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
+import "@fontsource-variable/inter";
 
 import "./index.css";
 import App from "./App.js";
